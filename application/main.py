1f9747515766d8470200817e811501b36cb6fd15
Jaden Myers <jadenmyers99@gmail.com>
2024-10-06 04:14:42 -0600

    bar fuckin downsky eh bud

--- application/main.py
+++ application/main.py
@@ -3,13 +3,14 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model.experiment import Model
 from flask import Flask, render_template, request
+import markdown
 
 app = Flask(__name__, template_folder="../view/templates")
 app.static_folder = "../static"
 model = Model()
-model.set_current_experiment("Schrodinger's Car"),
-model.set_current_experiment("Schrodinger's Truck"),
-model.set_current_experiment("Schrodinger's Van")
+model.set_current_experiment("SchrodingersCar"),
+model.set_current_experiment("SchrodingersTruck"),
+model.set_current_experiment("SchrodingersVan")
 experiments = None
 
 @app.route("/")
@@ -43,13 +44,14 @@ def detailedView():
     experiment = request.args.get("exp")
     model.set_current_experiment(experiment)
     model.set_current_version(version)
-    return render_template("detailed.html", version=version, experiment=experiment, model=model)
+    return render_template("detailed.html", render_markdown = render_markdown, version=version, experiment=experiment, model=model,)
 
 @app.route("/detailedView", methods=["POST"])
 def upload_file():
     if request.files["fileUpload"]:
         file = request.files['fileUpload']
-        upload_dir = '.\\res\\uploadedFiles'
+        upload_dir = os.path.join("res", "uploadedFiles")
+        # upload_dir = '.\\res\\uploadedFiles'
         file.save(os.path.join(upload_dir, file.filename))
         model.generate_ml_stats(os.path.join(upload_dir, file.filename))
     version = request.args.get("vrs")
@@ -58,6 +60,12 @@ def upload_file():
     model.set_current_version(version)
     return render_template("detailed.html", version=version, experiment=experiment, model=model)
     
+
+def render_markdown(text):
+    markdown_text = text
+    html_content = markdown.markdown(markdown_text)
+    return html_content
+
 
 @app.route("/addBranch")
 def addBranch():
